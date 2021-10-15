615bb5c05396b9c47753fd1e0f879c71cd840927
Esha2021 <eeswaricse@gmail.com>
2021-10-14 21:09:49 -0500

    spa menu validation completed

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,53 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Objects;
+
 public class User {
+
+    @NotBlank(message="Name is  required")
+    @Size(min=5,max=15, message="Name must be 5-15characters")
     private String username;
+
+    //@NotBlank(message="Email is required")
+    @Email(message="Invalid Try again")
     private String email;
+
+    @NotBlank(message="pwd is required")
+    @Size(min=6, message="pwd must be Six character long")
     private String password;
+    private int id;
+    private int nextId=1;
 
-    public User() {
+   @NotNull(message="password do not match")
+    private String verifyPassword;
 
-    }
 
-    public User(String username, String email, String password) {
-        this();
+    public User(String username, String email, String password, String verifyPassword, int id) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.id = nextId;
+        nextId++;
+
+    }
+   public User(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 
     public String getUsername() {
@@ -37,6 +71,18 @@ public class User {
     }
 
     public void setPassword(String password) {
+
         this.password = password;
+        checkPassword();
+
     }
+private void checkPassword(){
+    if (getPassword() != null && getVerifyPassword() != null && !password.equals(verifyPassword))
+
+    {
+          setVerifyPassword(null);
+      }
+}
+
+
 }
